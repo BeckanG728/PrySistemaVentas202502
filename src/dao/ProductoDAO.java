2485b866f7ac4d7be4d60f8c5ba7a9842ecc6c4e
Bkn <beckan0011@gmail.com>
2025-11-26 22:16:48 -0500

    Factura

--- src/dao/ProductoDAO.java
+++ src/dao/ProductoDAO.java
@@ -1,7 +1,6 @@
 package dao;
 
 // @author
-
 import conexionMySQL.ConexMySQL;
 import interfaz.VentasInferface;
 import java.io.File;
@@ -12,15 +11,15 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import to.ProductoTO;
 
-public class ProductoDAO implements VentasInferface<ProductoTO>{
+public class ProductoDAO implements VentasInferface<ProductoTO> {
 
     @Override
     public ResultSet search(Object nombreBuscar) throws Exception {
         Connection con = ConexMySQL.getInstance().getConnection();
-        String nombre = "%"+nombreBuscar+"%";
+        String nombre = "%" + nombreBuscar + "%";
         String sql = "select * from v_producto where nomb_prod like ?";
-        
-        PreparedStatement pst = con.prepareStatement(sql,ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
+
+        PreparedStatement pst = con.prepareStatement(sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
         pst.setString(1, nombre);
         ResultSet rs = pst.executeQuery();
         return rs;
@@ -29,13 +28,13 @@ public class ProductoDAO implements VentasInferface<ProductoTO>{
     public ResultSet searchID(int id) throws Exception {
         Connection con = ConexMySQL.getInstance().getConnection();
         String sql = "select * from v_producto where id_producto = ?";
-        
+
         PreparedStatement pst = con.prepareStatement(sql);
         pst.setInt(1, id);
         ResultSet rs = pst.executeQuery();
         return rs;
     }
-    
+
     @Override
     public void insert(ProductoTO objItem) throws Exception {
         Connection con = ConexMySQL.getInstance().getConnection();
@@ -44,7 +43,7 @@ public class ProductoDAO implements VentasInferface<ProductoTO>{
         cst.setString(1, objItem.getNombProd());
         cst.setDouble(2, objItem.getPrecProd());
         cst.setString(3, objItem.getObsvProd());
-        
+
         File objFile = new File(objItem.getFotoProd());
         FileInputStream objFileInputStream = new FileInputStream(objFile);
         cst.setBinaryStream(4, objFileInputStream);
@@ -60,7 +59,7 @@ public class ProductoDAO implements VentasInferface<ProductoTO>{
         cst.setString(2, objItem.getNombProd());
         cst.setDouble(3, objItem.getPrecProd());
         cst.setString(4, objItem.getObsvProd());
-        
+
         File objFile = new File(objItem.getFotoProd());
         if (objFile.exists()) {
             System.out.println("Existe");
